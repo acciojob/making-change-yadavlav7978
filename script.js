4e025a5c28595dffce6805ecafd44cadbb25e4ff
Lav Yadav <yadavlav571@gmail.com>
2024-05-04 08:28:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const makeChange = (c) => {
-  // your name here
+
+	const key=['q','d','n','p'];
+	const val=[25,10,5,1];
+
+	const num=parseInt(c);
+	const obj={ "q": 0, "d": 0, "n": 0, "p": 0 };
+
+	let i=0;
+	while(num>0){
+		while(val[i]>=num){
+			obj[key[i]]++;
+			num-=val[i];
+		}
+		i++;
+	}
+
+	return obj;
+	
+ 
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+const c = promp("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
